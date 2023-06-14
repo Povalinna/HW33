14ac147a79e2e569448a8a2b53e8530e748da277
Povalinna <111307356+Povalinna@users.noreply.github.com>
2023-06-14 21:45:24 +0300

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,14 +1,35 @@
 async function logJSONData() {
-
+  let answerData = {};
   let response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Kharkiv&units=metric&appid=2deaf2a2c50142d4cd22676b3542612c");
-  let answerData = await response.text();
+  answerData = await response.json();
   console.log(answerData);
+  
+  let city = answerData["name"];
+  let temp = answerData["main"]["temp"];
+  let pressure = answerData["main"]["pressure"];
+  let descr = answerData["weather"][0]["description"];
+  let hum = answerData["main"]["humidity"];
+  let speed = answerData["wind"]["speed"];
+  let deg = answerData["wind"]["deg"];
+  let allResult = `Mісто` + ` ` + city + `</br>` +
+                  'температура повітря' + ' ' + temp + `град` + `</br>` +
+                   `атмосферний тиск` + ` ` + pressure + 'гект-паск' + `</br>` +
+                   `опис` + ` ` + descr + `</br>` +
+                   `вологість` + ` ` + hum + '%' + `</br>` +
+                   `швидкість вітру` + ` ` + speed + 'м/сек' + `</br>` +
+                   `напрямок вітру` + ` ` + deg + 'град' + `</br>`;
+
+  let block = document.querySelector(`#container`);
+  let img = document.createElement(`img`);
+  img.src = "http://openweathermap.org/img/w/04d.png";
+  block.appendChild(img);
+  
   let result = document.createElement(`div`);
   result.setAttribute("id", "result");
-  result.innerHTML = answerData;
-
+  result.innerHTML = allResult;
   document.body.append(result);
 }
 logJSONData();
+
 
 
